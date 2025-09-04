5293a9dcabae11e3c344189fb4871c7c2d816564
SubhuBhai <subharthi2006@gmail.com>
2025-09-04 23:33:57 +0530

    added chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
-
+import City from './City'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className='bg-amber-700'>Hello</h1>
+    <div className='flex justify-center p-4 mt-15 space-x-8'>
+      
+    <City/>
+    <City/>
+    </div>
     </>
   )
 }
---
+++ src/City.jsx
@@ -0,0 +1,9 @@
+function City(){
+    return(
+        <>
+        <div className="h-[70vh] w-[50vh] bg-[#FFFFFF1A] border border-[#FFFFFF33] rounded-md"></div>
+        </>
+    );
+}
+
+export default City
