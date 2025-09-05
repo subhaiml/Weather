50bb53fdb9df9757939c9d5dfd2b51a2b74623f8
Aritra-Pal2006 <aritrapalofficial2006@gmail.com>
2025-09-05 19:31:04 +0530

    city created

--- src/City.jsx
+++ src/City.jsx
@@ -1,7 +1,14 @@
-function City(){
+function City(props){
     return(
         <>
-        <div className="h-[70vh] w-[50vh] bg-[#FFFFFF1A] border border-[#FFFFFF33] rounded-md"></div>
+        <div className="h-[50vh] space-y-5 w-[50vh] bg-[#FFFFFF1A]  text-white border-[#FFFFFF33] border-4 rounded-md items-center justify-center flex flex-col">
+
+        <p className=" text-8xl">{props.emoji}</p>
+
+        <p className="text-6xl">{props.country}</p>
+        <p className="text-3xl">{props.temp}</p>
+
+        </div>
         </>
     );
 }
